524fef08399e81ae731cf6af823d15c3d72e2511
Haseenasikkandar <haseenasikkandartce@gmail.com>
2017-02-15 14:34:32 +0530

    Update global.c

--- src/global.c
+++ src/global.c
@@ -70,8 +70,7 @@ float		CF=0.25,	/* confidence limit for tree pruning */
 
 Boolean		LOCK=false;	/* sample locked */
 
-double alpha=0.25;
-double q=1/(1-alpha);
+
 
 /*************************************************************************/
 /*									 */
